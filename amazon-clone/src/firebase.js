5b609b40966079977ca5e516ab478be64f5c41b0
Thomas Willson <thomaswilllson@yahoo.com>
2023-02-04 22:21:04 -0800

    First commit

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDhSUvM1V5mfktWE-VsZWJnIpFEtgv2hkg',
+  authDomain: 'challenge-f9b45.firebaseapp.com',
+  projectId: 'challenge-f9b45',
+  storageBucket: 'challenge-f9b45.appspot.com',
+  messagingSenderId: '500889108305',
+  appId: '1:500889108305:web:f4fd8e7a0db338e69edd70',
+  measurementId: 'G-DHFXMD0R4X',
+}
