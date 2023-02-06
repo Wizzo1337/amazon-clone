37f479b98165823375d1149dbbdf76d07bbf58d8
Thomas Willson <thomaswilllson@yahoo.com>
2023-02-05 18:19:04 -0800

    Finished Header

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     // BEM
     <div className="App">
-      <h1>Hello programmers, let's build an Amazon Store</h1>
       <Header/>
+      <Home/>
       {/* Home */}
     </div>
   );
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search'
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket'
 
 function Header() {
   return (
     <div className="header">
-      <img className="header__logo"
+      <img
+        className="header__logo"
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         alt="Amazon Logo"
       />
       <div className="header__search">
         <input className="header__searchInput" type="text" />
-        {/* Logo */}
+        <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
         <div className="header__option">
@@ -26,6 +29,11 @@ function Header() {
           <span className="header__optionLineTwo">Prime</span>
         </div>
         <div className="header__option"></div>
+
+				<div className="header__optionBasket">
+					<ShoppingBasketIcon/>
+					<span className='header__optionLineTwo header__basketCount'>0</span>
+				</div>
       </div>
     </div>
   )
