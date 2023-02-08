bf65ae5c40882d7c649b29e74ee1099b396f08b0
Thomas Willson <thomaswilllson@yahoo.com>
2023-02-08 11:31:07 -0800

    Updated basket

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
 import { useStateValue } from './StateProvider'
+import { getBasketTotal } from './reducer'
 
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue()
@@ -21,7 +22,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={basket.reduce((acc, ele) => acc + ele.price, 0)} //Part of the homework
+        value={getBasketTotal(basket)} //Part of the homework
         displayType={'text'}
         thousandSeparator={true}
         prefix={'$'}
