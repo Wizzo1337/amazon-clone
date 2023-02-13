1cf04fd2e907399db69f3af17130c549e8275ce4
Thomas Willson <thomaswilllson@yahoo.com>
2023-02-12 23:48:43 -0800

    testing to see if added to github

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,12 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCC5PNkqI3wybRsBbuwvnjgJOZke0rn4hQ',
+  apiKey: 'AIzaSyAqOlxA0w8Ulo7UjUvFxs2OJfAv_8N33yc',
   authDomain: 'challenge-f9b45.firebaseapp.com',
   projectId: 'challenge-f9b45',
   storageBucket: 'challenge-f9b45.appspot.com',
@@ -20,4 +19,4 @@ const firebaseApp = firebase.initializeApp(firebaseConfig)
 const db = firebaseApp.firestore()
 const auth = firebase.auth()
 
-export {db, auth}
+export { db, auth }
